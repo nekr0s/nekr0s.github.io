780e4ba2e8dd68fc26d1f592c1a586c50aa90399
Alexander Rusev <alexanderrusev@abv.bg>
2018-05-31 02:13:40 +0300

    Changed up links, adding wt, strelki and some coding sites

---
+++ homepage/rightclick.js
@@ -0,0 +1,10 @@
+document.onmousedown=disableclick;
+status="Right Click Disabled";
+function disableclick(event)
+{
+  if(event.button==2)
+   {
+     alert(status);
+     return false;    
+   }
+}
